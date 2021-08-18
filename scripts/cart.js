b7f62f3665585c1df1245428116c5103c15b4af9
Luyanda <LuyandaDingindlela@gmail.com>
2021-08-18 20:22:01 +0200

    cart functions complete

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,11 +1,58 @@
+let total_btn = document.querySelector(".total-btn")
+let clear_btn = document.querySelector(".clear-btn")
+
+total_btn.addEventListener("click", getTotal);
+clear_btn.addEventListener("click", clearCart);
+
+
 function getCart() {
     let cart = JSON.parse(localStorage.getItem("cart"))
     let container = document.querySelector(".cart-container")
 
+    container.innerHTML = "";
     cart.forEach(item => {
-        console.log(item[0]);
-        // container.innerHTML += item
+        let product = item[0]
+        container.innerHTML += renderCart(product);
     });
+}
+
+function removeFromCart(id) {
+    let item = getCartById(id)[0][0];
+    let cart = JSON.parse(localStorage.getItem("cart"))
+    let updated_cart = cart.filter(item => item[0].id != id);
+
+    localStorage.setItem("cart", JSON.stringify(updated_cart));
+
+    getCart();
+}
+
+function renderCart(product) {
+    return `
+            <div class="item" >
+                <h2>${product.name}</h2>
+                <h3>${product.description}</h3>
+                <p>${product.price}</p>
+                <button onclick="removeFromCart(${product.id})">Remove item</button>
+            </div>
+            `
+}
+
+function getCartById(id) {
+    let cart_items = JSON.parse(localStorage.getItem("cart"));
+    return cart_items.filter(product => product[0].id == id );
+}
+
+function getTotal() {
+    let total = 0
+    let cart = JSON.parse(localStorage.getItem("cart"))
+
+    cart.forEach(item => total += parseInt(item[0].price))
+
+    document.querySelector(".total").innerHTML = `the total is: R${total}`
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
 }
 
 
